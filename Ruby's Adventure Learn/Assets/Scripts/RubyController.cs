5db9d6920d23d1d52f6cdb07f41a6752c6c48361
zhyq <1064639871@qq.com>
2021-12-07 14:40:50 +0800

    角色移动+瓦片地图+阻止移动

---
+++ Ruby's Adventure Learn/Assets/Scripts/RubyController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    private Rigidbody2D _rubyRb;
+
+    private float _horizontal;
+
+    private float _vertical;
+
+    public int maxHealth;
+
+    private int _currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
+        _rubyRb = GetComponent<Rigidbody2D>();
+        _currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * _horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * _vertical * Time.deltaTime;
+        _rubyRb.MovePosition(position);
+    }
+    
+    public void ChangeHealth(int amount)
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth + amount, 0, maxHealth);
+        Debug.Log(_currentHealth + "/" + maxHealth);
+    }
+}
